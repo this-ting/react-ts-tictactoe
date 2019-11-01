7007cb84732f3d729e6ce967d783873650a646a2
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-11-01 20:23:39 +0800

    Set up inital Restart component

---
+++ src/Restart/Restart.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Restart.css";
+
+const Restart = () => {
+	return (
+		<>
+			<button className="Restart">Restart Game</button>
+		</>
+	);
+};
+
+export default Restart;
