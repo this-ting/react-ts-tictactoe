c6414a7efe70f644a4ec3750e581db709f841a24
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-11-04 01:14:41 +0800

    Set up renderSquare for Game component

--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -1,29 +1,44 @@
 import React, { Component } from "react";
 import "./Game.css";
 import Square from "../Square/Square";
+import PlayerContext from "../PlayerContext";
 
-class Game extends Component {
-	renderSquare() {
-		return <Square />;
-	}
+interface GameProps {
+	game: Array<string | null>;
+	handlePlayerClick: Function;
+}
+
+class Game extends Component<GameProps> {
+	static contextType = PlayerContext;
+	player = this.context;
+
+	renderSquare = (i: number) => {
+		const { handlePlayerClick } = this.props;
+		return (
+			<Square
+				input={this.props.game[i]}
+				handlePlayerClick={handlePlayerClick}
+			/>
+		);
+	};
 
 	render() {
 		return (
 			<div className="Game">
 				<div className="Game-HorizontalLine">
-					{this.renderSquare()}
-					{this.renderSquare()}
-					{this.renderSquare()}
+					{this.renderSquare(0)}
+					{this.renderSquare(1)}
+					{this.renderSquare(2)}
 				</div>
 				<div className="Game-HorizontalLine">
-					{this.renderSquare()}
-					{this.renderSquare()}
-					{this.renderSquare()}
+					{this.renderSquare(3)}
+					{this.renderSquare(4)}
+					{this.renderSquare(5)}
 				</div>
 				<div>
-					{this.renderSquare()}
-					{this.renderSquare()}
-					{this.renderSquare()}
+					{this.renderSquare(6)}
+					{this.renderSquare(7)}
+					{this.renderSquare(8)}
 				</div>
 			</div>
 		);
