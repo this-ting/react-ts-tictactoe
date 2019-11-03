45060d3fead4123c6407959b792819ed4b860374
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-11-04 01:14:02 +0800

    Set up SquareProps interface

--- src/Square/Square.tsx
+++ src/Square/Square.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import "./Square.css";
 
 interface SquareProps {
-	input: string;
-	handlePlayerClick: any;
+	input: null | string;
+	handlePlayerClick: Function;
 }
 
 const Square: React.FC<SquareProps> = ({ input, handlePlayerClick }) => {
 	return (
-		<div className="Square" onClick={handlePlayerClick}>
+		<div
+			className="Square"
+			onClick={() => {
+				handlePlayerClick();
+			}}
+		>
 			{input}
 		</div>
 	);
