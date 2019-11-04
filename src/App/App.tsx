bc14e1a00b593257cc2cf126dc5f3958f1d93ac1
Ting Chang <46632205+this-ting@users.noreply.github.com>
2019-11-04 11:51:03 +0800

    Set up function to declare winner

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Game from "../Game/Game";
+import GameOver from "../GameOver/GameOver";
 import Restart from "../Restart/Restart";
 import { PlayerProvider } from "../PlayerContext";
 
@@ -16,14 +17,36 @@ class App extends Component<AppProps, AppState> {
 		super(props);
 		this.state = {
 			player: "X",
-			game: [null, null, "X", null, null, null, null, null, null],
+			game: [null, null, null, null, null, null, null, null, null],
 		};
 	}
+
+	determineWinner: Function = (game: Array<string | null>) => {
+		const win: Array<Array<number>> = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+		for (let i = 0; i < win.length; i++) {
+			const [a, b, c] = win[i];
+			if (game[a] && game[a] === game[b] && game[a] === game[c]) {
+				console.log("winner " + game[a]);
+				return game[a];
+			}
+		}
+		return null;
+	};
 
 	handlePlayerClick = (event: Event, id: number) => {
 		console.log(id);
 		let squares: Array<string | null> = this.state.game.slice();
-		if (squares[id] === null) {
+		const winner: string | null = this.determineWinner(this.state.game);
+		if (squares[id] === null && !winner) {
 			squares[id] = this.state.player;
 			this.setState(state => ({
 				player: state.player === "X" ? "O" : "X",
@@ -40,12 +63,32 @@ class App extends Component<AppProps, AppState> {
 	};
 
 	render() {
-		return (
-			<PlayerProvider value={this.state.player}>
+		const winner = this.determineWinner(this.state.game);
+		if (!winner) {
+			return (
+				<PlayerProvider value={this.state.player}>
+					<div className="App">
+						<header className="App-header">
+							<h1>Tic Tac Toe</h1>
+							<h3>It is player {this.state.player}'s turn!</h3>
+						</header>
+						<body>
+							<Game
+								game={this.state.game}
+								handlePlayerClick={this.handlePlayerClick}
+							/>
+							<Restart handleRestart={this.handleRestart} />
+						</body>
+					</div>
+				</PlayerProvider>
+			);
+		} else {
+			return (
 				<div className="App">
 					<header className="App-header">
 						<h1>Tic Tac Toe</h1>
-						<h3>It is player {this.state.player}'s turn!</h3>
+						<h2>Player {winner} won!</h2>
+						<h3>Play again by clicking on "Restart Game"</h3>
 					</header>
 					<body>
 						<Game
@@ -55,8 +98,8 @@ class App extends Component<AppProps, AppState> {
 						<Restart handleRestart={this.handleRestart} />
 					</body>
 				</div>
-			</PlayerProvider>
-		);
+			);
+		}
 	}
 }
 
